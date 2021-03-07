f955f40c672c998bdeab55bc1138689728b9be82
Suranjan <suranjanbiswas2002@gmail.com>
2021-03-07 13:44:21 +0530

    Removed the styled jsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,3 @@
-//STYLED-JSX
-
-function Heading(props) {
-  return <div>
-    <h1>{props.heading}</h1>
-    <style jsx>
-      {`
-        h1{
-          color: red
-        }
-      `}
-    </style>
-  </div>
+export default function Home() {
+  return <h1>This is the index page!</h1>
 }
-
-export default function Home(){
-  return <div>
-   <Heading heading="This is the best!"/>
-  <h1>This is another heading!</h1>
- </div>
-} 
