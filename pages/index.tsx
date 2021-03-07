35cfaaec79159276fa24f1463db5d34458917ca9
Suranjan <suranjanbiswas2002@gmail.com>
2021-03-07 15:04:16 +0530

    Added the style modules to the app

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import styles from './index.module.css';
+
 export default function Home() {
-  return <h1>This is the index page!</h1>
+  return <h1 className={styles.headingStyle}>This is the index page!</h1>
 }
---
+++ pages/page1/index.tsx
@@ -0,0 +1,5 @@
+import styles from './styles.module.css';
+
+export default function FirstPage() {
+  return <h1 className={styles.firstPageHeading}>This is the first page!</h1>
+}
